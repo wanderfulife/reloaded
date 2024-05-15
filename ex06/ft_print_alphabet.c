e076fd48a21579e5a954b1c1b34009b380d52ac1
Jonathan Wander <jowander@bess-f4r2s5.clusters.42paris.fr>
2024-05-15 12:57:11 +0200

    findh

---
+++ ex06/ft_print_alphabet.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Jowander <jowander@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/14 15:05:39 by Jowander          #+#    #+#             */
+/*   Updated: 2024/05/14 15:05:41 by Jowander         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_alphabet(void)
+{
+	char	letter;
+
+	letter = 'a';
+	while (letter <= 'z')
+		ft_putchar(letter++);
+}
